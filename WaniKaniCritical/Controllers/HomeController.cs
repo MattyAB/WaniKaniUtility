2b621053c1c867857c7e046dc6fafa8881b126bd
Matt Beton <matthew.beton@gmail.com>
2018-01-05 17:26:40 +0000

    Initial Commit

---
+++ WaniKaniCritical/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WaniKaniCritical.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/critical/{key}")]
+        public IActionResult Critical(string key)
+        {
+            string json = GetStringFromURL("https://www.wanikani.com/api/user/" + key + "/critical-items/");
+
+            Models.JsonData data = JsonConvert.DeserializeObject<Models.JsonData>(json);
+
+            return View(data);
+        }
+
+        string GetStringFromURL(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Stream receiveStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(receiveStream, Encoding.UTF8);
+            return reader.ReadToEnd();
+        }
+    }
+}
---
+++ WaniKaniCritical/Models/JsonData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaniKaniCritical.Models
+{
+
+    public class UserInformation
+    {
+        public string username { get; set; }
+        public string gravatar { get; set; }
+        public int level { get; set; }
+        public string title { get; set; }
+        public string about { get; set; }
+        public string website { get; set; }
+        public string twitter { get; set; }
+        public int topics_count { get; set; }
+        public int posts_count { get; set; }
+        public int creation_date { get; set; }
+        public int? vacation_date { get; set; }
+    }
+
+    public class LearningPoint
+    {
+        public string type { get; set; }
+        public string character { get; set; }
+        public string meaning { get; set; }
+        public string onyomi { get; set; }
+        public string kunyomi { get; set; }
+        public string important_reading { get; set; }
+        public int level { get; set; }
+        public object nanori { get; set; }
+        public string percentage { get; set; }
+    }
+
+    public class JsonData
+    {
+        public UserInformation user_information { get; set; }
+        public List<LearningPoint> requested_information { get; set; }
+    }
+}
